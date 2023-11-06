7425fbc6ff1c79de24111e4f64517dbbf09dd008
MaciAC <macia.ac@gmail.com>
2023-11-06 21:00:12 +0100

    create models folder inside instance

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from os import path, mkdir
 from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+MODELS_DIR = "models"
 
 
 def create_app():
@@ -19,10 +20,12 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
-    
+    from .models import User
+
     with app.app_context():
         db.create_all()
+
+    create_storage_folders()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -35,7 +38,6 @@ def create_app():
     return app
 
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+def create_storage_folders():
+    if not path.exists('instance/' + MODELS_DIR):
+        mkdir('instance/' + MODELS_DIR)
