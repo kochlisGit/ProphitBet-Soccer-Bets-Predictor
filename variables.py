63a4ae0909c710cee33a4f89c629074a236cb678
Vasileios Kochliaridis <kohliaridis@Hotmail.com>
2024-01-29 10:28:23 +0200

    Delete variables.py

--- variables.py
+++
@@ -1,4 +0,0 @@
-available_leagues_filepath = 'database/storage/leagues/available_leagues.csv'
-saved_leagues_directory = 'database/storage/leagues/saved/'
-models_checkpoint_directory = 'database/storage/checkpoints/'
-random_seed = 0
