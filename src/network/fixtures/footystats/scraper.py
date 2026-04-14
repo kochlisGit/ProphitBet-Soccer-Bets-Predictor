63cde5163b5de19fd52277182dd6a77b2a7d7418
Vasileios Kochliaridis <kohliaridis@Hotmail.com>
2026-04-14 11:56:08 +0300

    Update scraper.py

--- src/network/fixtures/footystats/scraper.py
+++ src/network/fixtures/footystats/scraper.py
@@ -5,6 +5,9 @@ import pandas as pd
 from typing import Optional
 from lxml import html
 from selenium.webdriver import Chrome, Firefox, Edge, ChromeOptions, FirefoxOptions, EdgeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from src.network.netutils import check_internet_connection
 
 
@@ -49,14 +52,39 @@ class FootyStatsScraper:
         # Load webpage using the web driver.
         self._web_driver.get(url=fixture_url)
         self._web_driver.refresh()
-        for _ in range(int(self._page_load_timeout//self._poll_frequency)):
-            if self._web_driver.execute_script('return document.readyState') == 'complete':
-                break
-            time.sleep(self._poll_frequency)
+        # for _ in range(int(self._page_load_timeout//self._poll_frequency)):
+        #     if self._web_driver.execute_script('return document.readyState') == 'complete':
+        #         break
+        #     time.sleep(self._poll_frequency)
+
+        try:
+            WebDriverWait(self._web_driver, timeout=self._page_load_timeout).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'div.full-matches-table'))
+            )
+        except Exception as _:
+            logging.info('Timed out waiting for fixture table to load.')
+            return False
+        
+        time.sleep(1.0)        
         return True
 
     def parse_fixture_table(self, date_str: str) -> Optional[pd.DataFrame]:
         """ Parses the fixture table. The fixture table should be displayed on the web page! """
+
+        # Reads the odd from the provided span.
+        def get_odd(span) -> str:
+            text = span.text
+
+            # If text is inside child element, it attempts to read the text inside the child element instead.
+            # If no odd text is found, it returns 1.0, which id the default odd value.
+            if text is None:
+                for child in span:
+                    text = child.text
+
+                    if text is not None:
+                        break
+
+            return text.replace('\n', '').replace('\t', '') if text is not None else '1.0'
 
         tree = html.fromstring(self._web_driver.page_source)
         table_elements = tree.xpath('//div[contains(@class, "full-matches-table mt1e")]')
@@ -73,7 +101,8 @@ class FootyStatsScraper:
             if date_element is None:
                 continue
 
-            if date_element.text == formatted_date_str:
+            # if date_element.text == formatted_date_str:
+            if date_element.text_content().strip() == formatted_date_str:
                 requested_table = table
                 break
 
@@ -94,12 +123,12 @@ class FootyStatsScraper:
 
             # Parsing odds.
             odd_spans = ul.findall('li')[-1].xpath('.//span[contains(@class, "hover-modal-parent")]')
-            odd_1 = odd_spans[0].text.replace('\n', '').replace('\t', '')
-            odds_1.append(odd_1 if odd_1 != '' else 1.0)
-            odd_x = odd_spans[1].text.replace('\n', '').replace('\t', '')
-            odds_x.append(odd_x if odd_x != '' else 1.0)
-            odd_2 = odd_spans[2].text.replace('\n', '').replace('\t', '')
-            odds_2.append(odd_2 if odd_2 != '' else 1.0)
+            odd_1 = get_odd(span=odd_spans[0])
+            odds_1.append(odd_1)
+            odd_x = get_odd(span=odd_spans[1])
+            odds_x.append(odd_x)
+            odd_2 = get_odd(span=odd_spans[2])
+            odds_2.append(odd_2)
 
         # Add year to dates.
         df = pd.DataFrame({
